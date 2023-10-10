b0c7212b95a3340cbd591964f5f0f7262cfe1ea1
Dalecia <petersondalecia@gmail.com>
2023-10-10 13:17:46 -0400

    complete

---
+++ index.js
@@ -0,0 +1,70 @@
+function add(){
+    // defining add function
+}
+
+function subtract(){
+    //defining subtract function
+}
+
+function multiply(){
+    //defining multiply function
+}
+
+function divide(){
+    //define divide function
+}
+function add(){
+    return "something"
+}
+function add(a, b){
+    return (a + b)
+}
+
+function subtract(a, b){
+    return (a -b)
+}
+
+function multiply(a, b){
+    return (a * b)
+}
+
+function divide(a, b){
+    return (a / b)
+}
+function increment(){
+    //defining increment
+}
+function increment(n){
+    n++
+    return (n)
+}
+function decrement(){
+    //defininf decrement
+}
+function decrement(n){
+    n--
+    return (n)
+}
+
+function makeInt(){
+    //defining makeInt
+}
+function parseInt(){
+
+}
+function makeInt(n){
+  parseInt('n', 10)
+  return (n)
+}
+
+function preserveDecimal(){
+    //defining
+}
+function parseFloat(){
+
+}
+function preserveDecimal(n){
+    parseFloat(n) === 2.222
+    return (parseFloat(n))
+}
+
